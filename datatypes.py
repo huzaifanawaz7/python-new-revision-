01d792874b0560ce1718c88424c969ddb8a2172c
huzaifa <huzaifanawaz407@gmail.com>
2026-08-18 11:23:40 +0500

    try except

--- datatypes.py
+++ datatypes.py
@@ -1,12 +1,22 @@
-a=10
-int(a)
-print(type(a))
-a=10.0
-float(a)
-print(type(a))
-a="10"
-str(a)
-print(type(a))
-a=True
-print(type(a))
-bool(a)
+# a=10
+# int(a)
+# print(type(a))
+# a=10.0
+# float(a)
+# print(type(a))
+# a="10"
+# str(a)
+# print(type(a))
+# a=True
+# print(type(a))
+# bool(a)
+
+# name=input("enter your naem")
+# height=input("enter your height")
+# age=input("enter your age")
+
+try:
+    num=int(input("enter any number"))
+except ValueError:
+    print("you are only allowed to enter your number")
+print(f"the number you entered is {num}")
