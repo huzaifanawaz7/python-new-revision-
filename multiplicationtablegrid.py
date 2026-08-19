3c81fd91204b8ce368a6610a18f23639d9101bb5
huzaifa <huzaifanawaz407@gmail.com>
2026-08-19 12:25:23 +0500

    table grid

---
+++ multiplicationtablegrid.py
@@ -0,0 +1,6 @@
+# want to write a table from 1 to 10 through loops 
+for i in range(1,10):
+    for j in range(1,10):
+        print(i," * ", j ," = ",i*j)
+    print("*************************************")
+    
