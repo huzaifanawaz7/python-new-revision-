5619b2b03ae258c3b3de09a4afc5674eacc7e01f
huzaifa <huzaifanawaz407@gmail.com>
2026-08-19 11:45:34 +0500

    amstrongnumber

---
+++ amstrongnumber.py
@@ -0,0 +1,18 @@
+num=int(input("entr a number you want to check whther it is a amstrong or not"))
+orignal=num
+temp=num
+count=0
+while num>0:
+   num%10
+   count+=1
+   num//=10
+amstrong_sum=0
+
+while temp>0:
+   n1=temp%10
+   amstrong_sum+=n1**count
+   temp//=10
+if amstrong_sum==orignal:
+   print("the number is an amstrong number")
+else:
+   print("this number is not amstrong")
